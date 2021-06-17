c752f42d5c668883e0798bf321f6b153ae9f8457
arcec-cs <arcec.cs@gmail.com>
2021-06-17 15:31:15 -0400

    Added route "/catalog/author" that returns an alphabetized array of obj that contain the authors and their id

--- routes/catalog.js
+++ routes/catalog.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 function catalogRoutes(db) { //function so we can inject db dependency
   let router = express.Router();
-  // '/titles' route returns a Texts title, author, aategory and Ids for each so we may use them to make request form the fronend
+  // '/titles' route returns all Texts title, author, aategory and Ids for each so we may use them to make request form the fronend
   router
   .route('/titles')
   .get((req, res) => {
@@ -12,10 +12,21 @@ function catalogRoutes(db) { //function so we can inject db dependency
     .join('Categories', 'Categories.id', '=', 'Texts.category_id')
     .select('Texts.id', 'Texts.title', 'Authors.author', 'Categories.category', 'Texts.words', 'Texts.author_id', 'Texts.category_id')
     .orderBy('Texts.title')
-    .then(texts =>{
+    .then(texts => {
       res.status(200).json(texts);
     })
-    .catch(err => res.status(404).json('could not retrieve catalog'))
+    .catch(err => res.status(404).json('could not retrieve catalog'));
+  });
+  // '/authors' route returns a list of alphabetized authors and thier ids
+  router
+  .get('/authors',(req, res)=> {
+    db('Authors')
+    .select('Authors.author', 'Authors.id')
+    .orderBy('Authors.author')
+    .then(authorList => {
+      res.status(200).json(authorList);
+    })
+    .catch(err => res.status(404).json('could not retrieve authors id'));
   });
   
   return router;
