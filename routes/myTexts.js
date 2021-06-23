1b7d4b6e2541b0f40df6963c5f59997bde13ed53
arcec-cs <arcec.cs@gmail.com>
2021-06-23 19:53:05 -0400

    Added "myText/:uId/progress/:tId" route and PUT and GET method on that route.     The PUT method accepts a progress object in the body and updates the progress feild in ttDB that correlate with the given uId and tId.     The GET method sends the user the stored progress for the given tId and uId, it also updates the last_accessed field of the User_Texts table. While GET request typically does create data, I believe that this is an appropriate acception as a log.

--- routes/myTexts.js
+++ routes/myTexts.js
@@ -25,6 +25,39 @@ function myTextsRoutes(db) {
       })
     .catch(e => {console.log(e);res.status(404).json('uId was not specified')});
   });
+  
+  //Used in TypeTracerApp to update progress
+  router
+  .route('/:uId/progress/:tId')
+  .put((req, res) => {
+    const {uId, tId} = req.params; 
+    const {progress} = req.body;
+    //update progress
+    db('User_Texts')
+    .update({progress: progress})
+    .where({u_id: uId, t_id: tId})
+    .then(() => res.status(200).json('Success, progress was updated'))
+    .catch(e => res.status(404).json('unable to update user-texts'));
+  })
+  //used to get progress when starting typetracer app 
+  .get((req, res) => { 
+    const lastAccessed = Math.ceil(Date.now() / 1000); //milli to sec, always get a full int
+    const {uId, tId} = req.params;
+    //log access time
+    db('User_Texts') 
+    .update({last_accessed: lastAccessed})//unix time into seconds
+    .where({u_id: uId, t_id: tId})
+    .then(a => console.log('updated time')).catch(console.log(`Couldnt update ${uId} access time for ${tId}`));
+    //get and send progress
+    db('User_Texts')
+    .select('progress')
+    .where({u_id: uId, t_id: tId})
+    .then(progress => {
+      res.status(200).json(progress[0]) // returns {progress:{page:, para:, sen:, c_start}}
+    })
+    .catch(e => res.status(404).json('unable to get progress'));
+  });
+
 
   return router;
 }
