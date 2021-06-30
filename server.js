034d1d63c691626b7a4019c7ee6af428072ae7b0
arcec-cs <arcec.cs@gmail.com>
2021-06-30 13:58:42 -0400

    mend

--- server.js
+++ server.js
@@ -36,16 +36,3 @@ app.use('/signIn', signin.signinRoute(db, bcrypt, jwt));
 app.listen(3005, ()=>{
   console.log('app is running on port 3005');
 })
-
-// function authToken(req, res, next){
-//   const authHeader = req.headers['authorization']
-//   const token = authHeader && authHeader.split(' ')[1]
-//   if (token == null) return res.sendStatus(401)
-
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-//     console.log(err)
-//     if (err) return res.sendStatus(403)
-//     req.user = user
-//     next()
-//   })
-// }
